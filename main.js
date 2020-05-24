eac0412d38190085a709fe1aba0e990d72fdd8c0
p3t3r276 <p3t3r276@gmail.com>
2020-05-24 13:01:04 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on("ready", function() {
+  mainWindow = new BrowserWindow({});
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
+});
