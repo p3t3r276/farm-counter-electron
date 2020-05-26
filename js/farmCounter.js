4a6ca8894b67175a7cf0ae5eb4e3e59d9075c827
Peter Khoa Lê Minh <p3t3r276@gmail.com>
2020-05-26 16:00:12 +0700

    hoàn thành run btn và reset btn

--- js/farmCounter.js
+++ js/farmCounter.js
@@ -1,13 +1,26 @@
 const electron = require("electron");
 
 const { ipcRenderer } = electron;
+let runNum = 0;
 
 $(function () {
   $("#setFarm").on("click", function () {
     ipcRenderer.send("setFarm:open");
   });
+
+  $("#btnRun").on("click", function () {
+    runNum++;
+    $("#runNum").text(runNum);
+  });
+
+  $("#tbnResetFarm").on("click", function () {
+    runNum = 1;
+    $("#runNum").text(runNum);
+  });
 });
 
 ipcRenderer.on("item:set", function (e, item) {
   $("#itemName").text(item);
+  runNum = 1;
+  $("#runNum").text(runNum);
 });
